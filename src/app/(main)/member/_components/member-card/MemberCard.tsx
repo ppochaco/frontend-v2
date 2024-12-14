dcf254efc25259d428573928dde6f8aa7ffa3ce9
ajin <aj5210@naver.com>
2024-12-14 23:23:42 +0900

    Refactor(member): 소개글과 grid 추가

--- src/app/(main)/member/_components/member-card/MemberCard.tsx
+++ src/app/(main)/member/_components/member-card/MemberCard.tsx
@@ -39,17 +39,13 @@ export const MemberCard = ({
   }
 
   return (
-    <Card className="flex h-auto w-52 flex-col items-center justify-evenly p-4">
+    <Card className="flex h-auto w-40 flex-col items-center justify-evenly p-4 md:w-52">
       <div className="h-auto w-full transition-all duration-500 [transform-style:preserve-3d] [transform:perspective(800px)] hover:cursor-pointer hover:[transform:rotateY(180deg)]">
-        <Avatar className="aspect-4/3 h-auto w-full rounded-md [backface-visibility:hidden]">
+        <Avatar className="aspect-square h-auto w-full rounded-md object-cover [backface-visibility:hidden]">
           <AvatarImage src={userImageUrl} />
-          <AvatarFallback className="aspect-4/3 w-full rounded-md text-border">
+          <AvatarFallback className="aspect-square w-full rounded-md px-3 text-sm text-primary/30">
             프로필 사진이 없습니다
           </AvatarFallback>
-
-          <div className="absolute bottom-0 right-0 h-6 w-6 rounded-tl-sm bg-white">
-            <div className="absolute h-0 w-0 rounded-sm border-r-[26px] border-t-[26px] border-r-transparent border-t-slate-200"></div>
-          </div>
         </Avatar>
 
         <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center rounded-sm bg-accent px-3 [backface-visibility:hidden] [transform:rotateY(180deg)]">
@@ -58,11 +54,8 @@ export const MemberCard = ({
               {userDetail}
             </div>
           ) : (
-            <div className="text-border">한 줄 소개가 없습니다</div>
+            <div className="text-sm text-primary/30">한 줄 소개가 없습니다</div>
           )}
-          <div className="absolute bottom-0 left-0 h-6 w-6 rounded-tl-sm bg-white">
-            <div className="absolute h-0 w-0 rounded-sm border-l-[26px] border-t-[26px] border-l-transparent border-t-accent"></div>
-          </div>
         </div>
       </div>
       <div className="flex w-full items-center justify-end pt-2">
