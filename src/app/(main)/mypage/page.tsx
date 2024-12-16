2debf09df527223ce677ecf57627ac28678ee17d
Dobbymin <gmin990624@gmail.com>
2024-12-17 02:20:41 +0900

    Fix(mypage): 배열로 선언한 mockdata 객체로 수정

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -32,20 +32,18 @@ const MyPage = () => {
         onSubmit={() => onSubmit}
         className="mb-30 mt-1 w-full max-w-screen-xl px-12 pb-20 pt-10 md:mt-5 md:px-20"
       >
-        {mypageMockData.map((data) => (
-          <div key={data.userId}>
-            <UserImgSection
-              studentId={data.studentId}
-              name={data.name}
-              role={data.role}
-            />
-            <UserInfoSection
-              githubInfo={data.socialInfo.github}
-              instagramInfo={data.socialInfo.instagram}
-              profileIntro={data.introduction}
-            />
-          </div>
-        ))}
+        <div>
+          <UserImgSection
+            studentId={mypageMockData.studentId}
+            name={mypageMockData.name}
+            role={mypageMockData.role}
+          />
+          <UserInfoSection
+            githubInfo={mypageMockData.socialInfo.github}
+            instagramInfo={mypageMockData.socialInfo.instagram}
+            profileIntro={mypageMockData.introduction}
+          />
+        </div>
       </form>
     </Form>
   )
@@ -53,18 +51,16 @@ const MyPage = () => {
 
 export default MyPage
 
-const mypageMockData = [
-  {
-    introduction: '도비는 자유에요!',
-    userId: '3',
-    studentId: '2099111222',
-    name: '호반우',
-    role: '해구르르',
-    socialInfo: {
-      github: 'hobanwo',
-      instagram: 'hobanwo',
-    },
-    // profileImage: new File([], ''),
-    profileImage: 'https://github.com/shadcn.png',
+const mypageMockData = {
+  introduction: '도비는 자유에요!',
+  userId: '3',
+  studentId: '2099111222',
+  name: '호반우',
+  role: '해구르르',
+  socialInfo: {
+    github: 'hobanwo',
+    instagram: 'hobanwo',
   },
-]
+  // profileImage: new File([], ''),
+  profileImage: 'https://github.com/shadcn.png',
+}
