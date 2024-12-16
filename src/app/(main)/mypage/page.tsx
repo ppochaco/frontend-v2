f456317a209709e6bd73d2adefe59312132f6b66
Dobbymin <gmin990624@gmail.com>
2024-12-16 22:21:51 +0900

    Fix(mypage): mock data 적용

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -5,7 +5,7 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import { Form } from '@/components/ui'
-import { CreateMypageSchema } from '@/schema/mypage'
+import { CreateMypage, CreateMypageSchema } from '@/schema/mypage'
 
 import { UserImgSection, UserInfoSection } from './_components'
 
@@ -22,21 +22,49 @@ const MyPage = () => {
     },
   })
 
-  const onSubmit = (data: unknown) => {
+  const onSubmit = (data: CreateMypage) => {
     console.log(data)
   }
 
   return (
     <Form {...form}>
       <form
-        onSubmit={onSubmit}
+        onSubmit={() => onSubmit}
         className="mb-30 mt-1 w-full max-w-screen-xl px-12 pb-20 pt-10 md:mt-5 md:px-20"
       >
-        <UserImgSection />
-        <UserInfoSection />
+        {mypageMockData.map((data) => (
+          <div key={data.userId}>
+            <UserImgSection
+              studentId={data.studentId}
+              name={data.name}
+              role={data.role}
+            />
+            <UserInfoSection
+              githubInfo={data.socialInfo.github}
+              instagramInfo={data.socialInfo.instagram}
+              profileIntro={data.introduction}
+            />
+          </div>
+        ))}
       </form>
     </Form>
   )
 }
 
 export default MyPage
+
+const mypageMockData = [
+  {
+    introduction: '도비는 자유에요!',
+    userId: '3',
+    studentId: '2099111222',
+    name: '호반우',
+    role: '해구르르',
+    socialInfo: {
+      github: 'hobanwo',
+      instagram: 'hobanwo',
+    },
+    // profileImage: new File([], ''),
+    profileImage: 'https://github.com/shadcn.png',
+  },
+]
