01e41998d6aa8a1291e31469bccfc503dcf05e36
Dobbymin <gmin990624@gmail.com>
2024-12-16 22:11:05 +0900

    Fix(mypage): schema 수정

--- src/schema/mypage.ts
+++ src/schema/mypage.ts
@@ -6,7 +6,7 @@ export const CreateMypageSchema = z.object({
   introduction: z
     .string()
     .min(1, { message: '한 줄 소개를 입력해주세요.' })
-    .max(100, { message: '한 줄 소개는 100자 이내이어야 합니다.' }),
+    .max(100, { message: '한 줄 소개는 15자 이내이어야 합니다.' }),
 
   socialInfo: z.object({
     github: z.string().optional(),
@@ -21,7 +21,7 @@ export const CreateMypageSchema = z.object({
     .refine((file) => !!file.name, {
       message: '프로필 이미지를 선택해주세요.',
     })
-    .optional(), // 프로필 이미지는 선택 사항으로 처리
+    .optional(),
 })
 
 export type CreateMypage = z.infer<typeof CreateMypageSchema>
