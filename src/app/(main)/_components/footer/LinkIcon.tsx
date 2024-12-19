8aeab80f3dc8cb6682c17e9a30a80a205510a72c
Dobbymin <gmin990624@gmail.com>
2024-12-20 00:03:40 +0900

    Fix: Icon 타입 수정

--- src/app/(main)/_components/footer/LinkIcon.tsx
+++ src/app/(main)/_components/footer/LinkIcon.tsx
@@ -1,14 +1,17 @@
+import { ComponentType } from 'react'
+
 import {
   EnvelopeClosedIcon,
   GitHubLogoIcon,
   InstagramLogoIcon,
 } from '@radix-ui/react-icons'
+import { IconProps } from '@radix-ui/react-icons/dist/types'
 import Link from 'next/link'
 
 type LinkIconData = {
   id: number
   href: string
-  icon: JSX.Element
+  icon: ComponentType<IconProps>
   alt: string
 }
 
@@ -17,7 +20,7 @@ export const LinkIcon = () => {
     <div className="absolute bottom-8 right-12 flex gap-4">
       {linkIconData.map((linkIcon) => (
         <Link href={linkIcon.href} key={linkIcon.id}>
-          {linkIcon.icon}
+          <linkIcon.icon className="h-6 w-6" />
         </Link>
       ))}
     </div>
@@ -28,19 +31,19 @@ const linkIconData: LinkIconData[] = [
   {
     id: 0,
     href: 'mailto:knu.haedal@gmail.com',
-    icon: <EnvelopeClosedIcon className="h-6 w-6" />,
+    icon: EnvelopeClosedIcon,
     alt: 'HAEDAL Email Link',
   },
   {
     id: 1,
     href: 'https://github.com/KNU-HAEDAL',
-    icon: <GitHubLogoIcon className="h-6 w-6" />,
+    icon: GitHubLogoIcon,
     alt: 'HAEDAL Github Link',
   },
   {
     id: 2,
     href: 'https://www.instagram.com/knu.haedal/',
-    icon: <InstagramLogoIcon className="h-6 w-6" />,
+    icon: InstagramLogoIcon,
     alt: 'HAEDAL Instagram Link',
   },
 ]
