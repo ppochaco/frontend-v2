477ee9d4c8f8535d78568dc0578b31e0b25a8244
joojjang <itchu999@naver.com>
2024-12-19 23:43:47 +0900

    Design(events): 행사 게시판 디자인 변경 중

--- src/app/(main)/event/page.tsx
+++ src/app/(main)/event/page.tsx
@@ -1,23 +1,22 @@
-import Link from 'next/link'
-
-import { Button } from '@/components/ui'
-
-import { EventCarousel, EventHero } from './_components'
+// import Link from 'next/link'
+// import { Button } from '@/components/ui'
+// import { EventCarousel, EventHero } from './_components'
+import { EventHero } from './_components'
 
 const EventPage = () => {
   return (
-    <div className="flex h-full w-full flex-1 flex-col items-center gap-10 pb-20">
+    <div className="flex h-full w-full flex-1 flex-col items-center pb-20">
       <EventHero />
-      <div className="flex w-full flex-col gap-2 px-20">
+      {/* <div className="flex w-full flex-col px-20">
         <EventCarousel />
-        <div className="flex w-full justify-end pr-4">
+        <div className="flex justify-end">
           <Link href="/event/board">
             <Button variant="link" className="h-fit p-0 text-primary/60">
-              더보기
+              더 보기
             </Button>
           </Link>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
