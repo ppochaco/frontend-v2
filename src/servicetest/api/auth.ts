3ca744089d3486d0a4ecb3d83b54e01986dde7ab
ajin <aj5210@naver.com>
2024-12-20 18:57:23 +0900

    Refactor(auth): 로그인 api 수정
    - next-safe-action 제거, useMutation 사용
    - login api 호출 swagger-typescript-api 사용

--- src/servicetest/api/auth.ts
+++ src/servicetest/api/auth.ts
@@ -1,12 +1,10 @@
 import { Login, LoginRequestDto } from '@/models'
-import { BACKEND_API } from '@/servicetest/config'
-
-const loginApi = new Login({ axiosInstance: BACKEND_API.instance })
 
 export const login = async ({ userId, password }: LoginRequestDto) => {
+  const loginApi = new Login({})
   const response = await loginApi.signIn1({ userId, password })
 
   const accessToken = response.headers['authorization']
 
-  return { token: accessToken }
+  return accessToken
 }
