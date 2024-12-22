7eb0d85fc43ff2cb9f5ce2eb754abd18aa2c25f4
joojjang <itchu999@naver.com>
2024-12-23 08:27:51 +0900

    Feat(event): 인스타그램 영역 추가 중

--- src/app/(main)/event/page.tsx
+++ src/app/(main)/event/page.tsx
@@ -17,9 +17,9 @@ const EventPage = () => {
   const [view, setView] = useState<View>('갤러리')
 
   return (
-    <div className="flex h-full w-full flex-1 flex-col items-center pb-20">
+    <div className="flex h-full w-full flex-1 flex-col items-center">
       <EventHero />
-      <div className="flex w-full flex-col px-12">
+      <div className="flex w-full flex-col px-12 pb-20">
         <EventBoardHero />
         <EventViewSelector view={view} setView={setView} />
         {view === '갤러리' && <EventGallery />}
@@ -31,6 +31,9 @@ const EventPage = () => {
         <div className="flex w-full justify-end">
           <CreateEventPostButton />
         </div>
+      </div>
+      <div className="flex w-full items-center justify-center border-t-2 border-t-slate-100 px-12 py-10">
+        인스타그램 영역
       </div>
     </div>
   )
