853efa59bcb0198262350927a1a1116516db6a27
joojjang <itchu999@naver.com>
2024-12-23 14:22:35 +0900

    Feat(Instagram): 게시물 수 반응형 적용

--- src/app/(main)/event/_components/instagram/Instagram.tsx
+++ src/app/(main)/event/_components/instagram/Instagram.tsx
@@ -1,6 +1,31 @@
-import Image from 'next/image'
+'use client'
+
+import { useEffect, useState } from 'react'
+
+// import Image from 'next/image'
 
 export const Instagram = () => {
+  const [itemCount, setItemCount] = useState(12)
+
+  useEffect(() => {
+    const updateItemCount = () => {
+      if (window.innerWidth < 640) {
+        setItemCount(9)
+      } else {
+        setItemCount(12)
+      }
+    }
+
+    // 초기화 및 이벤트 리스너 설정
+    updateItemCount()
+    window.addEventListener('resize', updateItemCount)
+
+    // 이벤트 리스너 정리
+    return () => {
+      window.removeEventListener('resize', updateItemCount)
+    }
+  }, [])
+
   return (
     <div className="relative flex w-full flex-col items-center justify-center border-t-2 border-t-slate-100 px-12 pb-20 pt-8">
       <p className="text-semibold pointer text-xl leading-10">Instagram</p>
@@ -8,12 +33,13 @@ export const Instagram = () => {
         해달 인스타그램을 팔로우하고 다양한 소식을 접하실 수 있어요!
       </p>
       <div className="grid w-full grid-cols-3 items-center justify-center gap-2 sm:grid-cols-4 md:grid-cols-6">
-        {Array.from({ length: 10 }).map((_, index) => (
+        {Array.from({ length: itemCount }).map((_, index) => (
           <div
             key={index}
             className="flex aspect-square w-full items-center justify-center border border-gray-200 bg-slate-50"
           >
-            <Image src="" alt="" />
+            {index + 1}
+            {/* <Image src="" alt="" /> */}
           </div>
         ))}
       </div>
