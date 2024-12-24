199af7770ea5f828de465f579bf1d076f5324ac3
joojjang <itchu999@naver.com>
2024-12-25 06:07:27 +0900

    Chore(event): use client 위치 옮김
    - useState 쓰는 곳으로

--- src/app/(main)/event/_components/board-area/BoardArea.tsx
+++ src/app/(main)/event/_components/board-area/BoardArea.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Suspense, useState } from 'react'
 
 import { BoardHero as EventBoardHero } from './BoardHero'
--- src/app/(main)/event/page.tsx
+++ src/app/(main)/event/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Separator } from '@/components/ui'
 
 import { EventBoardArea, EventHero, EventInstagramArea } from './_components'
