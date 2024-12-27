ac2290c3bfd5e8a9421d8079c2086953d51afddf
Dobbymin <gmin990624@gmail.com>
2024-12-27 17:04:49 +0900

    Fix(mypage): 한줄소개, 소셜정보 form 기능 수정

--- src/app/(main)/mypage/_components/user-social-info/UserSocialInfoSection.tsx
+++ src/app/(main)/mypage/_components/user-social-info/UserSocialInfoSection.tsx
@@ -33,7 +33,7 @@ export const UserSocialInfoSection = ({
     },
   })
 
-  const { register, handleSubmit, setValue, watch } = form
+  const { register, handleSubmit, watch } = form
 
   const onSubmit = (data: CreateMypageSocialInfo) => {
     console.log('제출된 데이터:', data)
@@ -41,119 +41,115 @@ export const UserSocialInfoSection = ({
     setIsEditingSocial(false)
   }
 
+  const clickToEdit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    if (isEditingIntro === false && isEditingSocial === false) {
+      setIsEditingIntro(true)
+      setIsEditingSocial(true)
+    } else {
+      setIsEditingIntro(false)
+      setIsEditingSocial(false)
+    }
+  }
+
   return (
-    <Form {...form}>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="mt-10 flex h-full w-full flex-col space-y-5"
-      >
-        <div className="flex flex-col gap-5 md:flex-row md:justify-between">
-          <div className="flex flex-col gap-2 md:flex-row md:items-center md:gap-10">
-            <div className="text-lg font-bold text-primary md:text-xl">
-              한 줄 소개
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex h-full w-full flex-col space-y-5"
+        >
+          <div className="flex flex-row items-center justify-end gap-2">
+            <Button
+              variant="outline"
+              onClick={clickToEdit}
+              className="text-destructive"
+            >
+              수정
+            </Button>
+            <Button variant="default" type="submit">
+              완료
+            </Button>
+          </div>
+          <div className="flex flex-col gap-5 md:flex-row md:justify-between">
+            {/* 한 줄 소개 */}
+            <div className="flex flex-col gap-2 md:flex-row md:items-center md:gap-10">
+              <div className="text-lg font-bold text-primary md:text-xl">
+                한 줄 소개
+              </div>
+              <div className="md:y-0 y-5 flex h-10 gap-5">
+                {isEditingIntro ? (
+                  <Input
+                    className="w-full md:w-auto"
+                    {...register('introduction')}
+                    autoFocus
+                  />
+                ) : (
+                  <div className="text-md flex h-10 items-center">
+                    {watch('introduction')}
+                  </div>
+                )}
+              </div>
             </div>
-            <div className="md:y-0 y-5 flex h-10 gap-5">
-              {isEditingIntro ? (
-                <Input
-                  className="w-full md:w-auto"
-                  {...register('introduction')}
-                  autoFocus
-                />
-              ) : (
-                <div className="text-md flex h-10 items-center">
-                  {watch('introduction')}
+          </div>
+          <div className="text-sm text-zinc-400 md:text-base">
+            멤버 페이지에서 보이는 정보입니다.
+          </div>
+          <div className="border-t border-border" />
+
+          {/* 소셜 정보 */}
+          <div className="flex flex-col gap-5 md:flex-row md:justify-between">
+            <div className="flex flex-col gap-2">
+              <div className="text-lg font-bold text-primary md:text-xl">
+                소셜 정보
+              </div>
+              <div className="flex flex-col gap-3 md:flex-row md:gap-5 md:py-2">
+                <div className="flex h-10 items-center gap-2">
+                  <GitHubLogoIcon className="h-5 w-5" />
+                  {isEditingSocial ? (
+                    <Input
+                      className="w-full md:w-auto"
+                      {...register('githubInfo')}
+                    />
+                  ) : (
+                    <span>{watch('githubInfo')}</span>
+                  )}
                 </div>
-              )}
-            </div>
-          </div>
-          <Button
-            className="w-fit px-0 py-0 font-semibold text-destructive hover:text-destructive md:px-3 md:py-[2px]"
-            variant="ghost"
-            onClick={(e) => {
-              e.preventDefault()
-              if (isEditingIntro) {
-                handleSubmit(onSubmit)()
-              } else {
-                setIsEditingIntro(true)
-              }
-            }}
-          >
-            {isEditingIntro ? '완료' : '수정'}
-          </Button>
-        </div>
-        <div className="text-sm text-zinc-400 md:text-base">
-          멤버 페이지에서 보이는 정보입니다.
-        </div>
-        <div className="border-t border-border" />
-
-        <div className="flex flex-col gap-5 md:flex-row md:justify-between">
-          <div className="flex flex-col gap-2">
-            <div className="text-lg font-bold text-primary md:text-xl">
-              소셜 정보
-            </div>
-            <div className="flex flex-col gap-3 md:flex-row md:gap-5 md:py-2">
-              <div className="flex h-10 items-center gap-2">
-                <GitHubLogoIcon className="h-5 w-5" />
-                {isEditingSocial ? (
-                  <Input
-                    className="w-full md:w-auto"
-                    {...register('githubInfo')}
-                    autoFocus
-                  />
-                ) : (
-                  <span>{watch('githubInfo')}</span>
-                )}
-              </div>
-              <div className="flex h-10 items-center gap-2">
-                <InstagramLogoIcon className="h-5 w-5" />
-                {isEditingSocial ? (
-                  <Input
-                    className="w-full md:w-auto"
-                    {...register('instagramInfo')}
-                    autoFocus
-                  />
-                ) : (
-                  <span>{watch('instagramInfo')}</span>
-                )}
+                <div className="flex h-10 items-center gap-2">
+                  <InstagramLogoIcon className="h-5 w-5" />
+                  {isEditingSocial ? (
+                    <Input
+                      className="w-full md:w-auto"
+                      {...register('instagramInfo')}
+                    />
+                  ) : (
+                    <span>{watch('instagramInfo')}</span>
+                  )}
+                </div>
               </div>
             </div>
           </div>
-          <Button
-            className="w-fit px-0 py-0 font-semibold text-destructive hover:text-destructive md:px-3 md:py-[2px]"
-            variant="ghost"
-            onClick={(e) => {
-              e.preventDefault()
-              if (isEditingSocial) {
-                handleSubmit(onSubmit)()
-              } else {
-                setIsEditingSocial(true)
-              }
-            }}
-          >
-            {isEditingSocial ? '완료' : '수정'}
-          </Button>
-        </div>
-        <div className="border-t border-border" />
-
-        {/* Delete Account Section */}
-        <div className="flex flex-col gap-5 md:flex-row md:justify-between">
-          <div className="flex flex-col gap-2">
-            <div className="text-lg font-bold text-primary md:text-xl">
-              회원 탈퇴
-            </div>
-            <div className="text-sm text-zinc-400 md:text-base">
-              탈퇴 시 작성한 게시글 및 댓글이 모두 삭제되며 복구되지 않습니다.
-            </div>
+        </form>
+      </Form>
+      {/* 회원 탈퇴 */}
+      <div className="flex flex-col gap-5 md:flex-row md:justify-between">
+        <div className="flex flex-col gap-2">
+          <div className="text-lg font-bold text-primary md:text-xl">
+            회원 탈퇴
           </div>
-          <Button
-            className="w-fit px-0 py-0 font-semibold text-destructive hover:text-destructive md:px-3 md:py-[2px]"
-            variant="ghost"
-            onClick={() => console.log('회원 탈퇴')}
-          >
-            탈퇴하기
-          </Button>
+          <div className="text-sm text-zinc-400 md:text-base">
+            탈퇴 시 작성한 게시글 및 댓글이 모두 삭제되며 복구되지 않습니다.
+          </div>
         </div>
-      </form>
-    </Form>
+        <Button
+          className="w-fit px-0 py-0 font-semibold text-destructive hover:text-destructive md:px-3 md:py-[2px]"
+          variant="ghost"
+          onClick={() => console.log('회원 탈퇴')}
+        >
+          탈퇴하기
+        </Button>
+      </div>
+    </>
   )
 }
