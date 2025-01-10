96a149a61255a820a328c6adad41cd2f5962da54
ajin <aj5210@naver.com>
2025-01-10 21:52:36 +0900

    Rename(activity): 활동 게시글 단일 조회 관련 컴포넌트 구조 수정

--- src/app/(main)/activity/[semesterId]/[activityId]/boards/[boardId]/posts/[postId]/_components/detail/Detail.tsx
+++ src/app/(main)/activity/[semesterId]/[activityId]/boards/[boardId]/posts/[postId]/_components/detail/Detail.tsx
@@ -2,11 +2,13 @@ import { useMutation } from '@tanstack/react-query'
 import { format } from 'date-fns'
 import { usePathname, useRouter } from 'next/navigation'
 
-import { Button, Separator, useToast } from '@/components/ui'
+import { Separator, useToast } from '@/components/ui'
 import { queryClient } from '@/lib/query-client'
 import { activityPostQuries, deleteActivityPostApi } from '@/service/api'
 import { PostWithBoardResponseDto } from '@/service/models'
 import { useMyInfoStore } from '@/store/myInfo'
+
+import { DeletePostDialog } from './delete-dialog'
 
 type ActivityPostDetailProps = {
   boardId: number
@@ -17,7 +19,7 @@ export const ActivityPostDetail = ({
   boardId,
   post,
 }: ActivityPostDetailProps) => {
-  const { userName } = useMyInfoStore((state) => state.getMyInfo())
+  const { userId } = useMyInfoStore((state) => state.getMyInfo())
 
   const { mutate: deleteActivityPost, isPending } = useMutation({
     mutationFn: deleteActivityPostApi,
@@ -46,15 +48,11 @@ export const ActivityPostDetail = ({
     <div className="flex flex-col gap-3 py-4 text-primary">
       <div className="pt-4 text-4xl font-semibold">{post.postTitle}</div>
       <div className="flex justify-end">
-        {userName === post.userName && (
-          <Button
+        {userId === post.userId && (
+          <DeletePostDialog
             onClick={() => deleteActivityPost({ boardId, postId: post.postId })}
             disabled={isPending}
-            variant="link"
-            className="h-fit p-0 text-primary/60 hover:text-primary"
-          >
-            삭제하기
-          </Button>
+          />
         )}
       </div>
       <div className="flex flex-col items-start gap-1 text-sm sm:flex-row sm:items-end sm:justify-between">
