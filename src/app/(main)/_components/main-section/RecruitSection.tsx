64d553bd9b42c702b0ee0627959a7e6381b830ce
joojjang <itchu999@naver.com>
2025-01-11 12:00:10 +0900

    Feat(main): 이미지 삽입

--- src/app/(main)/_components/main-section/RecruitSection.tsx
+++ src/app/(main)/_components/main-section/RecruitSection.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import { Content } from '@/components/common'
 import { Button } from '@/components/ui'
 
@@ -5,8 +7,8 @@ export const RecruitSection = () => {
   return (
     <Content className="w-full bg-primary py-20 md:px-10">
       <div className="flex w-full flex-1 flex-col gap-10 text-lg text-white">
-        <div className="recruit-area flex w-full flex-col items-center">
-          <span
+        <div className="flex flex-col items-center px-12">
+          <p
             className="bg-gradient-to-r from-yellow-400 via-slate-100 to-yellow-600 bg-clip-text text-4xl font-semibold leading-loose text-transparent"
             style={{
               WebkitBackgroundClip: 'text',
@@ -14,18 +16,25 @@ export const RecruitSection = () => {
             }}
           >
             해달과 함께 성장해갈 동아리원을 모집합니다!
-          </span>
-          <span className="text-center text-slate-300">
+          </p>
+          <Image
+            src="/haedal-notebook.png"
+            width={0}
+            height={0}
+            className="h-auto sm:w-1/2 lg:w-1/3"
+            alt="Haedal Background"
+          />
+          <p className="text-center text-slate-300">
             지금은 모집 기간이 아닙니다.
             <br />
             2025년 1학기 모집 때 다시 만나요🌝
-          </span>
+          </p>
           <Button
             variant="secondary"
-            className="mt-10 h-fit text-2xl text-yellow-400"
+            className="mt-4 h-fit text-2xl text-yellow-400"
             onClick={() => (window.location.href = '/recruit')}
           >
-            지원하기
+            더 알아보기 →
           </Button>
         </div>
       </div>
--- src/app/(main)/_components/main-section/Section1.tsx
+++ src/app/(main)/_components/main-section/Section1.tsx
@@ -1,28 +1,44 @@
+import Image from 'next/image'
+
 import { Content } from '@/components/common'
 
 export const Section1 = () => {
   return (
-    <Content className="bg-primary px-10 py-40 text-white md:px-10">
-      <div className="flex w-full flex-1 flex-col gap-10">
-        <div className="flex w-full flex-col gap-2">
-          <div className="text-4xl">경북대학교 IT대학 학술동아리 </div>
-          <div className="text-5xl">HAEDAL</div>
-        </div>
-        <div className="flex flex-col gap-2 text-lg">
-          <div>
-            <strong>해달</strong>은{' '}
-            <strong className="bg-yellow-400 px-1 text-primary">
-              SW 가치 확산
-            </strong>
-            이라는 목표를 가지고 다양한 코딩 행사를 진행하는 동아리입니다.
+    <>
+      <Image
+        src="/haedal-background.png"
+        width={0}
+        height={0}
+        className="h-auto w-full"
+        alt="Haedal Background"
+      />
+      <Content
+        style={{ backgroundColor: '#010238' }}
+        className="flex flex-col items-center px-10 py-40 text-white"
+      >
+        <div className="flex w-full flex-1 flex-col gap-10">
+          <div className="flex w-full flex-col gap-2">
+            <div className="text-4xl">경북대학교 IT대학 학술동아리 </div>
+            <div className="text-5xl">HAEDAL</div>
           </div>
-          <div>
-            비전공자도 상관없이 코딩에 관심이 있는 사람이라면{' '}
-            <strong className="bg-yellow-400 px-1 text-primary">누구나</strong>{' '}
-            함께하실 수 있습니다.
+          <div className="flex flex-col gap-2 text-lg">
+            <div>
+              <strong>해달</strong>은{' '}
+              <strong className="bg-yellow-400 px-1 text-primary">
+                SW 가치 확산
+              </strong>
+              이라는 목표를 가지고 다양한 코딩 행사를 진행하는 동아리입니다.
+            </div>
+            <div>
+              비전공자도 상관없이 코딩에 관심이 있는 사람이라면{' '}
+              <strong className="bg-yellow-400 px-1 text-primary">
+                누구나
+              </strong>{' '}
+              함께하실 수 있습니다.
+            </div>
           </div>
         </div>
-      </div>
-    </Content>
+      </Content>
+    </>
   )
 }
