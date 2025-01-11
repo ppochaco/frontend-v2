82f0900ec990442d01f41247a3b2b9dc8a74f753
ajin <aj5210@naver.com>
2025-01-11 18:10:10 +0900

    Feat(post): 게시글 에디터에 로컬 이미지 업로드 기능 추가

--- src/components/feature/post/create-post-form/editor/EditorField.tsx
+++ src/components/feature/post/create-post-form/editor/EditorField.tsx
@@ -5,14 +5,35 @@ import { useFormContext } from 'react-hook-form'
 import { BlockNoteView } from '@blocknote/mantine'
 import '@blocknote/mantine/style.css'
 import { useCreateBlockNote } from '@blocknote/react'
+import { useMutation } from '@tanstack/react-query'
 
 import { FormField, FormItem, FormMessage } from '@/components/ui'
 import { CreateActivityPost } from '@/schema/post'
+import { uploadPostImageApi } from '@/service/api/post/image-upload'
 
 const PostContentFieldEditor = () => {
+  const { mutateAsync: uploadPostImage } = useMutation({
+    mutationFn: uploadPostImageApi,
+    onSuccess: (data) => onSuccess(data.postImageId),
+  })
   const { control } = useFormContext<CreateActivityPost>()
 
-  const editor = useCreateBlockNote()
+  const uploadFile = async (file: File): Promise<string> => {
+    const data = await uploadPostImage({ data: { file } })
+
+    return data.postImageUrl.replace(
+      '/upload',
+      'https://www.knu-haedal.com/api/upload',
+    )
+  }
+
+  const editor = useCreateBlockNote({
+    uploadFile,
+  })
+
+  const onSuccess = (imageId: number) => {
+    console.log(imageId)
+  }
 
   return (
     <FormField
