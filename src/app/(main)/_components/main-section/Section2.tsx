93e36fdeb35a46c2c5c461a95ebd2bae4848d1f3
joojjang <itchu999@naver.com>
2025-01-13 07:53:32 +0900

    Design(Section2): 아이템 중앙 정렬

--- src/app/(main)/_components/main-section/Section2.tsx
+++ src/app/(main)/_components/main-section/Section2.tsx
@@ -5,7 +5,7 @@ import { Carousel } from './carousel'
 export const Section2 = () => {
   return (
     <Content className="w-full bg-white py-20 md:px-10">
-      <div className="flex w-full flex-1 flex-col gap-10 text-lg text-primary">
+      <div className="flex w-full flex-1 flex-col items-center gap-10 text-lg text-primary">
         <Carousel />
       </div>
     </Content>
