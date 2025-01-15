39e10a24ea390550fd5d25ac9d877813485fff70
joojjang <itchu999@naver.com>
2025-01-16 01:08:12 +0900

    Design(carousel): 캐러셀 컴포넌트 CSS 수정

--- src/app/(main)/_components/main-section/carousel/Carousel.tsx
+++ src/app/(main)/_components/main-section/carousel/Carousel.tsx
@@ -20,44 +20,46 @@ export const Carousel = () => {
     <CarouselComponent
       opts={{ loop: true }}
       plugins={[Autoplay({ delay: 2000 })]}
-      className="w-full max-w-52 xs:max-w-sm sm:max-w-xl"
+      className="flex w-full max-w-52 flex-col items-center xs:max-w-sm sm:max-w-xl"
     >
-      <CarouselContent>
-        {Events.map((item, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="relative flex aspect-video flex-col p-0">
-                  <div className="flex w-full flex-1 items-center justify-center overflow-hidden rounded-xl">
-                    <Image
-                      alt={item.title}
-                      src={item.imageUrl}
-                      width="0"
-                      height="0"
-                      sizes="100vw"
-                      className="h-auto w-full"
-                    />
-                  </div>
-                  <div className="absolute bottom-0 left-0 right-0 flex flex-col gap-2 break-keep rounded-b-lg bg-gradient-to-t from-slate-100 via-slate-100/90 to-transparent px-6 pb-4 pt-8">
-                    <strong className="absolute right-6 top-8 bg-yellow-400 px-0.5 text-primary">
-                      {item.month}
-                    </strong>
-                    <p className="text-xl font-semibold leading-normal">
-                      {item.title}
-                    </p>
-                    <p className="text-md text-slate-600 lg:text-lg">
-                      {item.description}
-                    </p>
-                  </div>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
+      <div className="flex flex-row items-center gap-3">
+        <CarouselPrevious />
+        <CarouselContent>
+          {Events.map((item, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="relative flex aspect-video flex-col p-0">
+                    <div className="flex w-full flex-1 items-center justify-center overflow-hidden rounded-xl">
+                      <Image
+                        alt={item.title}
+                        src={item.imageUrl}
+                        width="0"
+                        height="0"
+                        sizes="100vw"
+                        className="h-auto w-full"
+                      />
+                    </div>
+                    <div className="absolute bottom-0 left-0 right-0 flex flex-col gap-2 break-keep rounded-b-lg bg-gradient-to-t from-slate-100 via-slate-100/90 to-transparent px-6 pb-4 pt-8">
+                      <strong className="absolute right-6 top-8 bg-yellow-400 px-0.5 text-primary">
+                        {item.month}
+                      </strong>
+                      <p className="text-xl font-semibold leading-normal">
+                        {item.title}
+                      </p>
+                      <p className="text-md text-slate-600 lg:text-lg">
+                        {item.description}
+                      </p>
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselNext />
+      </div>
       <CarouselDots className="pt-4" />
-      <CarouselPrevious />
-      <CarouselNext />
     </CarouselComponent>
   )
 }
