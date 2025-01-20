bca6abe66717cf95a2bd15eff1e7147b205ebc3d
ajin <aj5210@naver.com>
2025-01-20 15:24:00 +0900

    Fix: 게시글 생성, 수정 시 imageNames 추출 후 저장

--- frontend/src/service/schema/post.ts
+++ frontend/src/service/schema/post.ts
@@ -6,7 +6,7 @@ export const CreateActivityPostSchema = z.object({
     .min(1, { message: '게시글 제목을 입력해주세요.' })
     .max(50, { message: '게시글 제목은 50자 이내여야 합니다.' }),
   postContent: z.string().min(1, { message: '게시글 내용을 입력해주세요.' }),
-  postImageIds: z.number().array(),
+  postImageNames: z.string().array(),
   postActivityStartDate: z.string().refine((date) => date !== undefined, {
     message: '활동 날짜를 선택해주세요.',
   }),
@@ -20,6 +20,6 @@ export const CreateNoticePostSchema = z.object({
     .min(1, { message: '게시글 제목을 입력해주세요.' })
     .max(50, { message: '게시글 제목은 50자 이내여야 합니다.' }),
   postContent: z.string().min(1, { message: '게시글 내용을 입력해주세요.' }),
-  postImageIds: z.number().array(),
+  postImageNames: z.string().array(),
 })
 export type CreateNoticePost = z.infer<typeof CreateNoticePostSchema>
