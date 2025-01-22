8813b9f0db5e911124f08f900d3a237dbf60ac96
ajin <aj5210@naver.com>
2025-01-22 16:59:27 +0900

    Remove: 사용하지 않는 주석 제거

--- frontend/src/pages/activity/edit-board/_components/hero/Hero.tsx
+++ frontend/src/pages/activity/edit-board/_components/hero/Hero.tsx
@@ -1,6 +1,5 @@
 import { useLocation } from 'react-router'
 
-// import { NotFound } from '@/components/common'
 import { NavLink } from '@/components/common'
 import { Separator } from '@/components/ui'
 import { ActivityBreadcrumb } from '@/pages/activity/components'
@@ -29,13 +28,3 @@ export const EditBoardHero = ({ boardName }: BoardHeroProps) => {
     </div>
   )
 }
-
-// const EditBoardHeroSkeleton = () => {
-//   return (
-//     <div>
-//       <Separator variant="dark" />
-//       <ActivityBreadcrumb navLinks={[]} pageName="게시판 수정하기" />
-//       <Separator variant="dark" />
-//     </div>
-//   )
-// }
