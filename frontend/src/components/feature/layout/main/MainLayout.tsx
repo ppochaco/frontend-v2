1a65ed99b7134916a525c00522b18f774de30981
ajin <aj5210@naver.com>
2025-01-26 15:19:25 +0900

    Refactor: 관리자 layout 제거

--- frontend/src/components/feature/layout/main/MainLayout.tsx
+++ frontend/src/components/feature/layout/main/MainLayout.tsx
@@ -1,12 +1,18 @@
+import { ReactNode } from 'react'
+
 import { Footer } from './footer'
 import { Header } from './header'
 
-export const MainLayout = ({ children }: { children: React.ReactNode }) => {
+interface MainLayoutProps {
+  children: ReactNode
+}
+
+export const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <main className="flex min-h-screen w-full flex-col">
       <Header />
-      <div className="mt-16 flex w-full flex-1 justify-center px-5 pt-10 sm:px-20">
-        <div className="max-w-screen-xl flex-1">{children}</div>
+      <div className="flex w-full flex-1 justify-center">
+        <div className="mt-16 max-w-screen-2xl flex-1">{children}</div>
       </div>
       <Footer />
     </main>
--- frontend/src/routes/custom-route/main/MainRoute.tsx
+++ frontend/src/routes/custom-route/main/MainRoute.tsx
@@ -5,7 +5,9 @@ import { MainLayout } from '@/components/feature/layout/main'
 export const MainRoute = () => {
   return (
     <MainLayout>
-      <Outlet />
+      <div className="flex w-full flex-1 justify-center px-5 pt-10 sm:px-20">
+        <Outlet />
+      </div>
     </MainLayout>
   )
 }
