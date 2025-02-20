8a925fa33c123bc52532e28b9c87b4b26f04d808
ajin <aj5210@naver.com>
2025-02-20 22:45:15 +0900

    Chore: Archivo Black 폰트 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -82,6 +82,9 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      fontFamily: {
+        archivo: ['Archivo Black', 'sans-serif'],
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
