2a0f4551c8172c1ccc086cbe421be87b44815588
ajin <aj5210@naver.com>
2025-02-23 14:50:04 +0900

    Design(recruit): 신청 버튼 크기 수정

--- frontend/src/pages/recruit/components/link-button/LinkButton.tsx
+++ frontend/src/pages/recruit/components/link-button/LinkButton.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui'
 export const RecruitLinkButton = () => {
   return (
     <div className="flex justify-center bg-gradient-to-t from-[#F1F5F9] to-[#E9EDFF] py-20">
-      <Button size="lg">
+      <Button className="text-md px-8 py-6">
         <div>신청 폼 바로가기</div>
         <ChevronRightCircle />
       </Button>
