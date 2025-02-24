09abf72fe031a6c18a2ae1a9bf771c4ae0d6c692
ajin <aj5210@naver.com>
2025-02-24 23:11:10 +0900

    Fix(recruit): 모집하기 버튼 리다이렉트 window.location으로 수정

--- frontend/src/pages/recruit/components/link-button/LinkButton.tsx
+++ frontend/src/pages/recruit/components/link-button/LinkButton.tsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router'
-
 import { ChevronRightCircle } from 'lucide-react'
 
 import { StarIcon } from '@/components/feature'
@@ -11,13 +9,19 @@ interface RecruitLinkButtonProps {
 }
 
 export const RecruitLinkButton = ({ className }: RecruitLinkButtonProps) => {
+  const handleClick = () => {
+    window.location.href =
+      'https://docs.google.com/forms/d/e/1FAIpQLSdhvW34Cscw1XceELYSccDKgBeAOw26D7wviNw9hsC3TMCYvw/viewform'
+  }
+
   return (
-    <Link to="https://forms.gle/74xngwMadKWfco647">
-      <Button className={cn('text-md gap-4 p-6', className)}>
-        <StarIcon />
-        <div className="font-bold">신청 폼 바로가기</div>
-        <ChevronRightCircle />
-      </Button>
-    </Link>
+    <Button
+      className={cn('text-md gap-4 p-6', className)}
+      onClick={handleClick}
+    >
+      <StarIcon />
+      <div className="font-bold">신청 폼 바로가기</div>
+      <ChevronRightCircle />
+    </Button>
   )
 }
