5a5b155b3b3ab1092986ffebefcde1b4dca1c3e1
ajin <aj5210@naver.com>
2025-02-24 19:00:17 +0900

    Fix(recruit): 행사 카드 정렬 수정

--- frontend/src/pages/recruit/components/event/description-card/DescriptionCard.tsx
+++ frontend/src/pages/recruit/components/event/description-card/DescriptionCard.tsx
@@ -16,9 +16,11 @@ export const DescriptionCard = ({
       style={{ backgroundColor }}
       className="h-full w-full border-none bg-gradient-to-t from-black/30 to-transparent font-semibold"
     >
-      <div className="flex aspect-[0.95] flex-col justify-between gap-10 p-3 transition-opacity duration-300 sm:p-5">
-        <div className="text-sm sm:text-base md:text-lg">{description}</div>
-        <div className="whitespace-pre-line text-lg text-white sm:text-xl md:text-2xl">
+      <div className="flex h-full flex-col justify-between gap-4 p-3 transition-opacity duration-300 sm:aspect-[0.96] sm:gap-0 md:p-5">
+        <div className="h-full flex-1 text-sm sm:text-base md:text-base">
+          {description}
+        </div>
+        <div className="whitespace-pre-line text-lg text-white sm:text-xl md:text-xl">
           {title}
         </div>
       </div>
