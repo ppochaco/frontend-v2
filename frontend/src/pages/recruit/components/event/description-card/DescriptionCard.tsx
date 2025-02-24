b2292e51ced40e4a6c9077f4c635c9ca79e7a789
ajin <aj5210@naver.com>
2025-02-24 19:28:46 +0900

    Fix(recruit): 모바일 뷰 깨짐 수정

--- frontend/src/pages/recruit/components/event/description-card/DescriptionCard.tsx
+++ frontend/src/pages/recruit/components/event/description-card/DescriptionCard.tsx
@@ -14,10 +14,10 @@ export const DescriptionCard = ({
   return (
     <Card
       style={{ backgroundColor }}
-      className="h-full w-full border-none bg-gradient-to-t from-black/30 to-transparent font-semibold"
+      className="h-full min-h-[180px] w-full border-none bg-gradient-to-t from-black/30 to-transparent font-semibold"
     >
-      <div className="flex h-full flex-col justify-between gap-4 p-3 transition-opacity duration-300 sm:aspect-[0.96] sm:gap-0 md:p-5">
-        <div className="h-full flex-1 text-sm sm:text-base md:text-base">
+      <div className="flex h-full flex-col justify-between gap-4 p-3 sm:aspect-[0.96] sm:gap-0 md:p-5">
+        <div className="h-full text-sm sm:text-base md:text-base">
           {description}
         </div>
         <div className="whitespace-pre-line text-lg text-white sm:text-xl md:text-xl">
