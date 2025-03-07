854fc4557b43030ffffd17dbd11bb3da2c8e74ed
ajin <aj5210@naver.com>
2025-03-07 23:22:32 +0900

    Design(mypage): 프로필 정보 글자 크기 반응형 추가

--- frontend/src/pages/mypage/_components/profile-info/ProfileInfo.tsx
+++ frontend/src/pages/mypage/_components/profile-info/ProfileInfo.tsx
@@ -9,12 +9,16 @@ export const ProfileInfo = ({ profile }: ProfileInfoProps) => {
   return (
     <div className="mb-10 flex flex-col gap-3">
       <div className="flex flex-row items-center gap-3">
-        <div className="text-2xl font-bold md:text-3xl">{profile.userName}</div>
-        <div className="text-md rounded-full border border-primary px-3 py-0.5 md:text-lg">
+        <div className="text-xl font-bold sm:text-2xl md:text-3xl">
+          {profile.userName}
+        </div>
+        <div className="rounded-full border border-primary px-2 py-0.5 text-sm sm:px-3 sm:text-base md:text-lg">
           {convertRoleName(profile.role)}
         </div>
       </div>
-      <div className="text-lg md:text-xl">{profile.studentNumber}</div>
+      <div className="text-base sm:text-lg md:text-xl">
+        {profile.studentNumber}
+      </div>
     </div>
   )
 }
