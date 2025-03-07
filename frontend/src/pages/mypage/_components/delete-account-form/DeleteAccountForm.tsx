52bc60da1d281a56f9c02e684aa9205a4753f5f4
ajin <aj5210@naver.com>
2025-03-07 23:52:31 +0900

    Fix(mypage): 회원 탈퇴 문구 수정

--- frontend/src/pages/mypage/_components/delete-account-form/DeleteAccountForm.tsx
+++ frontend/src/pages/mypage/_components/delete-account-form/DeleteAccountForm.tsx
@@ -5,13 +5,14 @@ export const DeleteAccountForm = () => {
     <div className="flex w-full flex-col justify-between">
       <div className="text-xl font-bold sm:text-2xl">회원 탈퇴</div>
       <Separator className="my-2 sm:my-3" />
-      <div>
+      <div className="text-xs text-primary/40 sm:text-sm">
+        탈퇴 후에도 작성하신 게시글과 댓글은 삭제되지 않습니다. 탈퇴 전에 직접
+        삭제해 주세요.
+      </div>
+      <div className="pt-2">
         <Button variant="secondary" onClick={() => console.log('회원 탈퇴')}>
           탈퇴하기
         </Button>
-      </div>
-      <div className="py-2 text-xs text-primary/40 sm:text-sm">
-        탈퇴 시 작성한 게시글 및 댓글이 모두 삭제되며 복구되지 않습니다.
       </div>
     </div>
   )
