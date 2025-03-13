9d5bc4bea03d1edd65f5e5b6a0806646998079c2
ajin <aj5210@naver.com>
2025-03-13 16:42:51 +0900

    Fix: 멤버 페이지 해구르르, 멤버만 보이게 설정

--- frontend/src/pages/member/MemberPage.tsx
+++ frontend/src/pages/member/MemberPage.tsx
@@ -1,25 +1,33 @@
 import { IntersectionObserverLoader } from '@/components/common'
 import { useProfileSuspensePaging } from '@/service/api'
-import { Role } from '@/types'
 
 import { MemberCard } from './components'
 
 export default function MemberPage() {
-  const roles: Role[] = ['ROLE_ADMIN', 'ROLE_MEMBER', 'ROLE_TEAM_LEADER']
+  const { data: admin } = useProfileSuspensePaging({ roles: ['ROLE_ADMIN'] })
 
-  const { data, hasNextPage, isFetchingNextPage, fetchNextPage } =
-    useProfileSuspensePaging({ roles })
+  const {
+    data: member,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useProfileSuspensePaging({ roles: ['ROLE_MEMBER'] })
 
-  const profiles = data?.pages.flatMap((page) => page.profiles)
+  const adminProfiles = admin?.pages.flatMap((page) => page.profiles)
+  const memberProfiles = member?.pages.flatMap((page) => page.profiles)
 
   return (
     <main className="flex h-full w-full flex-col items-center pb-20">
-      <div className="text-xl font-semibold">2024-2 멤버</div>
+      <div className="text-xl font-semibold">2025-1 멤버</div>
       <div className="text-md text-primary/60">
-        해달과 {profiles?.length}명의 부원들이 함께 하고 있어요
+        해달과 {adminProfiles.length + memberProfiles.length}명의 부원들이 함께
+        하고 있어요
       </div>
-      <div className="grid w-full max-w-[320px] grid-cols-2 place-items-center gap-6 pt-10 sm:max-w-[520px] sm:grid-cols-3 md:max-w-[680px] lg:max-w-[920px] lg:grid-cols-4">
-        {profiles?.map((user) => {
+      <div className="w-full max-w-[920px] pb-4 pt-10 text-xl font-semibold">
+        해구르르
+      </div>
+      <div className="grid w-full max-w-[320px] grid-cols-2 place-items-center gap-6 sm:max-w-[520px] sm:grid-cols-3 md:max-w-[680px] lg:max-w-[920px] lg:grid-cols-4">
+        {adminProfiles?.map((user) => {
           return (
             <MemberCard
               key={user.userId}
@@ -32,14 +40,39 @@ export default function MemberPage() {
           )
         })}
       </div>
-      {hasNextPage && (
-        <IntersectionObserverLoader
-          callback={() => {
-            if (!isFetchingNextPage) {
-              fetchNextPage()
-            }
-          }}
-        />
+      <div className="w-full max-w-[920px] pb-4 pt-10 text-xl font-semibold">
+        정회원 & 준회원
+      </div>
+      {memberProfiles.length ? (
+        <div>
+          <div className="grid w-full max-w-[320px] grid-cols-2 place-items-center gap-6 sm:max-w-[520px] sm:grid-cols-3 md:max-w-[680px] lg:max-w-[920px] lg:grid-cols-4">
+            {memberProfiles.map((user) => {
+              return (
+                <MemberCard
+                  key={user.userId}
+                  userName={user.userName}
+                  userImageUrl={user.profileImageUrl}
+                  userDetail={user.profileIntro}
+                  githubId={user.githubAccount}
+                  instagramId={user.instaAccount}
+                />
+              )
+            })}
+          </div>
+          {hasNextPage && (
+            <IntersectionObserverLoader
+              callback={() => {
+                if (!isFetchingNextPage) {
+                  fetchNextPage()
+                }
+              }}
+            />
+          )}
+        </div>
+      ) : (
+        <div className="w-full max-w-[920px] text-primary/60">
+          멤버가 없습니다.
+        </div>
       )}
     </main>
   )
