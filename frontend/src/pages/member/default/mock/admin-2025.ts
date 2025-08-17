411eba3dc28221b683e3a5b99cc91a8bc3e24b61
ajin <aj5210@naver.com>
2025-08-17 20:05:28 +0900

    Feat: 2025 해구르르 data, 타입 추가

---
+++ frontend/src/pages/member/default/mock/admin-2025.ts
@@ -0,0 +1,32 @@
+import { Admin2025 } from '@/types'
+
+export const admins2025: Admin2025[] = [
+  {
+    position: '회장',
+    userId: 'haedal2025',
+  },
+  {
+    position: '부회장',
+    userId: 'ezzkim1',
+  },
+  {
+    position: '총무',
+    userId: 'song123',
+  },
+  {
+    position: '교육운영진장',
+    userId: 'churaly',
+  },
+  {
+    position: '트랙조직위원장',
+    userId: 'ysh0702',
+  },
+  {
+    position: '홍보부장',
+    userId: 'pigmal1',
+  },
+  {
+    position: '기술관리부장',
+    userId: 'rnjs5540',
+  },
+]
